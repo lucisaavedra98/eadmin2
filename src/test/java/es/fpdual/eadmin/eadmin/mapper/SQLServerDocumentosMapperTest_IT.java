84b71c85fa971602b8983584f0624fe745f55ba8
lucisaavedra98 <lucisaavedr98@gmail.com>
2018-04-12 11:10:05 +0200

    Configurado

--- src/test/java/es/fpdual/eadmin/eadmin/mapper/SQLServerDocumentosMapperTest_IT.java
+++ src/test/java/es/fpdual/eadmin/eadmin/mapper/SQLServerDocumentosMapperTest_IT.java
@@ -2,15 +2,13 @@ package es.fpdual.eadmin.eadmin.mapper;
 
 import org.junit.runner.*;
 import org.springframework.test.context.*;
-import org.springframework.test.context.junit.*;;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({
+	"classpath:spring-configuracion/eadmin-persistencia.xml",
+	"classpath:spring-configuracion/eadmin-sqlserver-persistencia.xml"
+	})
 public class SQLServerDocumentosMapperTest_IT extends BaseDocumentoMapperTest {
 	
 }
-/*
-@RunWith(SpringJUnit4ClassRunner.clas)
-@ContextConfiguration({
-	"",
-	""
-	})
-*/
